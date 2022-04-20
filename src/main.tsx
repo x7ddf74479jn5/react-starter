b1e8f1f9c1d92477968a07887ea53bd07f02d47a
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-04-20 22:56:42 +0900

    🚨 chore

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("Root element not found");
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
